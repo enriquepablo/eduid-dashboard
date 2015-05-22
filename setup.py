6cab3ecf6f49e31c6dc67cdb4b165aed5add62fb
Enrique Pérez Arnaud <enriquepablo@users.noreply.github.com>
2015-05-22 14:36:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 version = '0.3.9b2'
 
 requires = [
-    'eduid_am>=0.5.3,<0.6.0-dev',
-    'eduid_msg>=0.8.7,<0.9.0-dev',
+    'eduid_am>=0.5.3',
+    'eduid_msg>=0.8.7',
     'vccs_client>=0.4.1',
     'eduid_lookup_mobile>=0.0.4',
     'eduid_userdb>=0.0.2',
